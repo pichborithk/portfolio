f89ca020d0efb8a4416ef36be6277b8a1193bdbc
Pichborith Kong <pichborith@gmail.com>
2023-11-06 20:38:06 -0800

    send email function

--- index.js
+++ index.js
@@ -25,3 +25,29 @@ themeButton.addEventListener('click', function () {
   document.body.classList.toggle('light-mode');
   themeButton.classList.toggle('dark');
 });
+
+/* ------------------------------- Send Email ------------------------------- */
+const sendBtn = document.querySelector('#send-email');
+const nameInput = document.querySelector('#contact-name');
+const emailInput = document.querySelector('#contact-email');
+const subjectInput = document.querySelector('#email-subject');
+const contentInput = document.querySelector('#email-content');
+
+sendBtn.addEventListener('click', event => {
+  event.preventDefault();
+
+  const params = {
+    name: nameInput.value,
+    email: emailInput.value,
+    subject: subjectInput.value,
+    body: contentInput.value,
+  };
+  emailjs.send('service_lq1lh4k', 'template_1aw8fgx', params).then(res => {
+    console.log(res.status);
+    alert('Success!');
+    nameInput.value = '';
+    emailInput.value = '';
+    subjectInput.value = '';
+    contentInput.value = '';
+  });
+});
